325a82d19bfbfb6f72123cc1d74a7362a2bf0112
Rafael Vastag <rgvastag@gmail.com>
2021-01-18 21:53:30 -0300

    CORS universal

--- SalesSystem/src/main/java/com/br/rafaelvastag/controller/ClienteController.java
+++ SalesSystem/src/main/java/com/br/rafaelvastag/controller/ClienteController.java
@@ -23,7 +23,6 @@ import com.br.rafaelvastag.model.repository.ClienteRepository;
 
 @RestController
 @RequestMapping("/api/clientes")
-@CrossOrigin("http://localhost:4200")
 public class ClienteController {
 
 	private final ClienteRepository clienteRepository;
--- SalesSystem/src/main/java/com/br/rafaelvastag/controller/ServicoPrestadoController.java
+++ SalesSystem/src/main/java/com/br/rafaelvastag/controller/ServicoPrestadoController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
